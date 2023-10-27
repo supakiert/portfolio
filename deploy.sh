5415bd95e795554031e801f4a8b2c28a26d886e2
newdrip <supakiert18442@gmail.com>
2023-10-28 01:44:31 +0700

    update

--- deploy.sh
+++ deploy.sh
@@ -21,5 +21,5 @@ git commit -m 'deploy'
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com: <USERNAME>/<USERNAME>.github.io.git main
 # if you are deploying to https://<USERNAME>.github.io/<REPO> 
-# git push -f git@github.com:supakiert/portfolio.git main:gh-pages
+# git push -f git@github.com:supakiert/newdripsomuch.git main:gh-pages
 cd -
